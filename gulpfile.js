53e5bd10d95f6ba7eade8d86f23b97a83594b057
Henry Tao <hi@henrytao.me>
2014-12-13 12:11:36 +0700

    Updated test coverage

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var reporters = require('jasmine-reporters');
 
-gulp.task('test', function() {
-  return gulp.src('test/spec/**/*.js')
-    .pipe(plugins.jasmine({
-      reporter: new reporters.JUnitXmlReporter({
-      	savePath: 'test/results/'
-      })
-    }));
+gulp.task('test', function(cb) {
+  gulp.src('test/spec/**/*.js')
+    .pipe(plugins.istanbul())
+    .pipe(plugins.istanbul.hookRequire())
+    .on('finish', function() {
+      gulp.src(['test/spec/**/*.js'])
+        .pipe(plugins.jasmine({
+          reporter: new reporters.JUnitXmlReporter({
+            savePath: 'test/results/'
+          })
+        }))
+        .pipe(plugins.istanbul.writeReports({
+        	dir: './test/results/coverage/'
+        }))
+        .on('end', cb);
+    });
 });
